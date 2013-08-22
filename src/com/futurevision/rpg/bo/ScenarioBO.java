915d05565ac902f8294cb865db648b8e315326b1
Pedro Veronezi <pedroveronezi@gmail.com>
2013-08-22 13:33:24 -0300

    Gerando BO

--- src/com/futurevision/rpg/bo/ScenarioBO.java
+++ src/com/futurevision/rpg/bo/ScenarioBO.java
@@ -2,10 +2,20 @@ package com.futurevision.rpg.bo;
 
 import java.io.Serializable;
 
-public class ScenarioBO implements Serializable{
-	
-	public void fugir(){
-		
+import com.futurevision.rpg.entity.Scenario;
+
+public class ScenarioBO implements Serializable {
+
+	public Scenario flee() {
+
+		return null;
+	}
+
+	public Scenario getScenarioById(Long id) {
+
+		return null;
 	}
 	
+	
+
 }
